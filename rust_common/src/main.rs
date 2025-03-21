50e083e1ec9bd6861ddd7921f3554f9d7dcfa99d
sid12214737 <uzmir6358@gmail.com>
2025-03-21 17:13:56 +0900

    ownerships

--- rust_common/src/main.rs
+++ rust_common/src/main.rs
@@ -1,18 +1,19 @@
 fn main() {
-    let s = String::from("hello");
-    
-    takes_ownership(s);
+    let s1 = gives_ownership();
 
-    let x = 5;
+    let s2 = String::from("hello");
 
-    makes_copy(x);
-    println!("{}", x);
-} 
-
-fn takes_ownership(some_string: String) {
-    println!("{some_string}");
+    let s3 = takes_and_gives_back(s2);
 }
 
-fn makes_copy(some_int: i32) {
-    println!("{some_int}");
+fn gives_ownership() -> String {
+
+    let some_string = String::from("yours");
+
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+
+    a_string
 }
