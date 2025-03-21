f1a0eb237225ca939f2a5fe83059b85ecc1b2853
sid12214737 <uzmir6358@gmail.com>
2025-03-21 17:27:35 +0900

    referencing/borrowing

--- rust_common/src/main.rs
+++ rust_common/src/main.rs
@@ -1,19 +1,11 @@
 fn main() {
-    let s1 = gives_ownership();
+    let mut s1 = String::from("hello");
 
-    let s2 = String::from("hello");
+    change(&mut s1);
 
-    let s3 = takes_and_gives_back(s2);
+    println!("{s1}");
 }
 
-fn gives_ownership() -> String {
-
-    let some_string = String::from("yours");
-
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-
-    a_string
+fn change(s: &mut String) {
+    s.push_str(", world!");
 }
