8774179fcdc6d80c4bdb5b47aa4c548c7a6490b9
sid12214737 <uzmir6358@gmail.com>
2025-03-21 17:10:13 +0900

    ownerships

--- rust_common/src/main.rs
+++ rust_common/src/main.rs
@@ -1,27 +1,18 @@
-use std::io;
-
 fn main() {
-    // FizzBuzz
-    println!("Fizz Buzz");
-    let mut x = String::new();
-    println!("Please enter number: ");
-    io::stdin()
-        .read_line(&mut x)
-        .expect("Failed to read line!");
+    let s = String::from("hello");
+    
+    takes_ownership(s);
 
-    let x: u32 = x
-        .trim()
-        .parse()
-        .expect("Please enter a valid number!");
+    let x = 5;
 
-    if x % 3 == 0 && x % 5 == 0 {
-        println!("FizzBuzz");
-    } else if x % 3 == 0 {
-        println!("Fizz");
-    } else if x % 5 == 0 {
-        println!("Buzz");
-    } else {
-        println!("{x}");
-    }
+    makes_copy(x);
+    println!("{}", x);
+} 
 
+fn takes_ownership(some_string: String) {
+    println!("{some_string}");
+}
+
+fn makes_copy(some_int: i32) {
+    println!("{some_int}");
 }
