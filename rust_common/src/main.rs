0f46f53b939ce8ffd2468130a0e87a3b48750d13
sid12214737 <uzmir6358@gmail.com>
2025-03-25 09:30:09 +0900

    tuple structs

--- rust_common/src/main.rs
+++ rust_common/src/main.rs
@@ -1,9 +1,14 @@
+//normal structure
 struct User {
     active: bool,
     username: String,
     email: String,
     sign_in_count: u64,
 }
+
+//tuple struct
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
     let user1 = User {
@@ -18,6 +23,10 @@ fn main() {
         email: String::from("mail"),
         ..user1
     };
-
+    // we can access email bs we dont unpack/move it to new user 
     println!("{}", user1.email);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    
 }
