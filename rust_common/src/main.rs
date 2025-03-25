2e368067d4c3d15051bbe15c684dd85bd66dc107
sid12214737 <uzmir6358@gmail.com>
2025-03-25 09:31:56 +0900

    unit-like struct

--- rust_common/src/main.rs
+++ rust_common/src/main.rs
@@ -10,6 +10,9 @@ struct User {
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 
+//unit-like struct
+struct AlwaysEqual;
+
 fn main() {
     let user1 = User {
         active: true,
@@ -28,5 +31,6 @@ fn main() {
 
     let black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
-    
+
+    let subject = AlwaysEqual;
 }
