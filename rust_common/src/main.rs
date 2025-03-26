db9d4d25a65607d967f9a46005114688ba3f9439
sid12214737 <uzmir6358@gmail.com>
2025-03-26 12:48:14 +0900

    enums insances can have different types of data, enums can also have associated impl block

--- rust_common/src/main.rs
+++ rust_common/src/main.rs
@@ -3,6 +3,19 @@ enum IpAddr {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move{x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn call(&self) {
+        //-snip-
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(String::from("127.0.0.1"));
 
